2b04c431ce3f9b2600d3950f1f87e01a76995972
P Manya (INTERN) <MPP1@dsone.3ds.com>
2025-11-24 12:07:37 +0530

    added template page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse("<h1>Hello Sir, this is my Django website hosted successfully!</h1>")
+    return render(request, 'index.html')
